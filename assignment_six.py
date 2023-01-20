ccf60747a17f8c05a0ca273b926528f7f162ef2f
Josh <goldsjo@ssfs.org>
2023-01-20 14:02:02 -0500

    Commit

--- assignment_six.py
+++ assignment_six.py
@@ -1,13 +1,24 @@
-def getbirthdays():
-    import random
+
+
+
+numpeople = 23
+possiblebdays = 365
+numtrials = userinput
 
 def userinput():
     userinput = ("Welcome to the birthday paradox.")
     useri = input("How many times would you like to run the simulation? Numbers only please.")
     if useri in (int):
-        break
+
     elif useri in():
         print("Please enter a number.")
+
+def getbirthdays():
+    import random
+    birthday = randint(1, possiblebdays)
+        return birthday
+
+
 
 birthddays = []
 
@@ -16,7 +27,7 @@ birthddays = []
  #   print(random.randint(0, 365))
 #birthdays = []
 
-#count
+
 
 
 
